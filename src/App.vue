1062875aa7e46a5fa613c5a5a919d4e091dee39b
Kirill Romanov <djaler1@gmail.com>
2021-10-04 12:16:34 +0300

    feat: initial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <Child :value="innerValue"/>
+</template>
+
+<script lang="ts">
+    import { defineComponent, ref } from '@vue/composition-api';
+    import Child from '@/Child.vue';
+
+    export default defineComponent({
+        name: 'App',
+        components: { Child },
+        setup() {
+            const innerValue = ref<unknown>(null);
+
+            return {
+                innerValue,
+            };
+        },
+    });
+</script>
---
+++ src/Child.vue
@@ -0,0 +1,17 @@
+<template>
+    {{value}}
+</template>
+
+<script lang="ts">
+    import { defineComponent, PropType } from '@vue/composition-api';
+
+    export default defineComponent({
+        name: 'Child',
+        props: {
+            value: {
+                type: Object as PropType<unknown>,
+                default: null,
+            },
+        },
+    });
+</script>
